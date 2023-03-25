0ee5c79ea2c5a2fa356256a901c6b403eb2dde22
SedlarDavid <sedlardavid.cz@gmail.com>
2023-03-25 14:44:50 +0100

    seeders added

---
+++ database/seeders/SectionSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class SectionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('sections')->insert([
+            'title' => "Praktické rady",
+            'description' => "Něco random o tom jaké systémy, věci, lifehacky na univerzitě provozujeme, něco jakoby možná vlastně smysluplného a zajímavého co tu nebude 2x nakopírované aby to bylo dost dlouhé."
+       ,'image'=> ""
+        ]);
+        DB::table('sections')->insert([
+            'title' => "Fakulty",
+            'description' => "Něco random o tom jaké m8 univeryita fakulty, ústavy, oddělení (Jobko, atd...), něco jakoby možná vlastně smysluplného a zajímavého co tu nebude 2x nakopírované aby to bylo dost dlouhé."
+            ,'image'=> ""
+        ]);
+        DB::table('sections')->insert([
+            'title' => "Studentské organizace",
+            'description' => "Něco random o tom jaké studentské organizace pod UTB fungují, kooperují, něco jakoby možná vlastně smysluplného a zajímavého co tu nebude 2x nakopírované aby to bylo dost dlouhé."
+            ,'image'=> ""
+        ]);
+    }
+}
