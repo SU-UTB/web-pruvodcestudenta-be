1f80d27a76ae35965253dbd7843bc03ce9a27ee2
SedlarDavid <sedlardavid.cz@gmail.com>
2023-04-04 20:31:20 +0200

    auth controller added

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $fields = $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|string|unique:users,email',
+            'password' => 'required|string|confirmed',
+
+        ]);
+
+        $user = User::create([
+            'name' => $fields['name'],
+            'email' => $fields['email'],
+            'password' => bcrypt($fields['password'])
+        ]);
+
+        $token = $user->createToken('authToken')->plainTextToken;
+
+        $response = [
+            'user' => $user,
+            'token' => $token
+        ];
+
+        return response($response, 201);
+    }
+    /**
+     * @OA\Post(
+     *   tags={"Authentication"},
+     *   path="/api/login",
+     *   summary="Logs in",
+     *     @OA\RequestBody(
+     *         @OA\MediaType(
+     *             mediaType="application/json",
+     *             @OA\Schema(
+
+     *                 @OA\Property(
+     *                     property="email",
+     *                     type="string"
+     *                 ),
+     *                @OA\Property(
+     *                     property="password",
+     *                     type="string"
+     *                 ),
+
+     *                 example={ "email": "sedlar@sutb.cz", "password": "examplePasword"}
+     *             )
+     *         )
+     *     ),
+     *   @OA\Response(response=200, description="OK"),
+     *   @OA\Response(response=401, description="Unauthorized"),
+     *   @OA\Response(response=404, description="Not Found")
+     * )
+     */
+    public function login(Request $request)
+    {
+        $fields = $request->validate([
+            'email' => 'required|string',
+            'password' => 'required|string',
+
+        ]);
+
+
+        //Check email
+        $user = User::where('email', $fields['email'])->first();
+
+        //Check password
+        if (!$user || !Hash::check($fields['password'], $user->password)) {
+
+
+            return response([
+                'message' => 'Bad credentials.'
+            ], 401);
+        }
+
+
+        $token = $user->createToken('authToken')->plainTextToken;
+
+        $response = [
+            'user' => $user,
+            'token' => $token
+        ];
+
+        return response($response, 201);
+    }
+    /**
+     * @OA\Post(
+     *   tags={"Authentication"},
+     *   path="/api/logout",
+     *   summary="Logs out",
+
+     *   @OA\Response(response=200, description="OK"),
+     *   @OA\Response(response=401, description="Unauthorized"),
+     *   @OA\Response(response=404, description="Not Found")
+     * )
+     */
+    public function logout(Request $request)
+    {
+
+        auth()->user()->tokens()->delete();
+
+        return ['message' => 'Logged out.'];
+    }
+}
