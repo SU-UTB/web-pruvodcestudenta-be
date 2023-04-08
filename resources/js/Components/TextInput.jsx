bf01f6bb824d143132040e2a3c1b83dac7ec014d
SedlarDavid <sedlardavid.cz@gmail.com>
2023-04-08 15:10:10 +0200

    admin sections

--- resources/js/Components/TextInput.jsx
+++ resources/js/Components/TextInput.jsx
@@ -1,6 +1,12 @@
-import { forwardRef, useEffect, useRef } from 'react';
+import {forwardRef, useEffect, useRef} from 'react';
 
-export default forwardRef(function TextInput({ type = 'text', className = '', isFocused = false, ...props }, ref) {
+export default forwardRef(function TextInput({
+                                                 type = 'text',
+                                                 className = '',
+                                                 isFocused = false,
+                                                 isDisabled = false,
+                                                 ...props
+                                             }, ref) {
     const input = ref ? ref : useRef();
 
     useEffect(() => {
@@ -11,14 +17,16 @@ export default forwardRef(function TextInput({ type = 'text', className = '', is
 
     return (
         <div className="flex flex-col items-start">
-            <input
-                {...props}
-                type={type}
-                className={
-                    'border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm ' +
-                    className
-                }
-                ref={input}
+            <input disabled={isDisabled}
+                   {...props}
+                   type={type}
+                   className={
+                       isDisabled ?
+                           'border-gray-300 bg-gray-100 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm '
+                           :'border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm ' +
+                       className
+                   }
+                   ref={input}
             />
         </div>
     );
