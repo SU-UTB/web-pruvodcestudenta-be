eeddb2ce9829dd43ebccf0c3d4759c53834a1497
SedlarDavid <sedlar@cngroup.dk>
2023-06-09 10:28:56 +0200

    bladewindui

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,44 +1,12 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<x-app-layout>
+    <x-bladewind.centered-content size="tiny" >
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+        <x-bladewind.card class="m-auto">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
-
-    <!-- Fonts -->
-    <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
-
-    <!-- Scripts -->
-    <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
-          integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-
-
-</head>
-
-<body>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
-</script>
-<div class="min-h-screen bg-gray-100">
-
-    <x-navbar></x-navbar>
-    <!-- Page Content -->
-    <main>
-        <div class="container px-3 py-3 m-0">
             <h6>Sections: {{ $countOfSections }}</h6>
             <h6>Topics: {{ $countOfTopics}}</h6>
+        </x-bladewind.card>
 
-        </div>
-    </main>
-</div>
+    </x-bladewind.centered-content>
 
-
-</body>
-
-
-</html>
+</x-app-layout>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,12 +11,24 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+              integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+
+
+        <link href="{{ asset('vendor/bladewind/css/animate.min.css') }}" rel="stylesheet" />
+
+        <link href="{{ asset('vendor/bladewind/css/bladewind-ui.min.css') }}" rel="stylesheet" />
+
+        <script src="{{ asset('vendor/bladewind/js/helpers.js') }}"></script>
+
+        <script src="//unpkg.com/alpinejs" defer></script>
+
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100">
-            @include('layouts.navigation')
+    <body class="font-sans antialiased" style="min-height: 100vh">
+        <div class="bg-gray-100" style="min-height: 100vh">
+            @include('components.navbar')
 
             <!-- Page Heading -->
             @if (isset($header))
