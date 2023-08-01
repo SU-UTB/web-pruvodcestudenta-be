f49138a623bd40f813965f2f1bc941e6a2045023
SedlarDavid <sedlardavid.cz@gmail.com>
2023-08-01 14:14:15 +0200

    meta

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'PruvodceStudenta | Admin') }}</title>
+    <title>{{ config('app.name', 'PruvodceStudenta UTB | Admin') }}</title>
 
     <link rel="stylesheet" href="{{URL::secureAsset('css/bootstrap.min.css')}}">
 
