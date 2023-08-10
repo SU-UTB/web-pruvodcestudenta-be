3c13a73b25471e2e865089ac97ded7b3d10c4ec1
SedlarDavid <sedlardavid.cz@gmail.com>
2023-08-10 09:34:36 +0200

    slug creation in topics

--- resources/views/components/add-topic-modal.blade.php
+++ resources/views/components/add-topic-modal.blade.php
@@ -33,11 +33,31 @@
             name="title"
             required
             placeholder="Title"
-
+            onchange="getSlug(this)"
         />
-        <br/>
+        <script type="text/javascript">
+            function getSlug(input1) {
 
-        <label for="editor"></label>
+                var input2 = document.getElementById('slug');
+                var str = input1.value;
+                str = str.replace(/^\s+|\s+$/g, ''); // trim
+                str = str.toLowerCase();
+
+                // remove accents, swap ñ for n, etc
+                var from = "ãàáäâẽèéëêìíïîõòóöôùúüûñç·/_,:;";
+                var to = "aaaaaeeeeeiiiiooooouuuunc------";
+                for (var i = 0, l = from.length; i < l; i++) {
+                    str = str.replace(new RegExp(from.charAt(i), 'g'), to.charAt(i));
+                }
+
+                str = str.replace(/[^a-z0-9 -]/g, '') // remove invalid chars
+                    .replace(/\s+/g, '-') // collapse whitespace and replace by -
+                    .replace(/-+/g, '-'); // collapse dashes
+
+                input2.value = str;
+            }
+        </script>
+
         <textarea
             rows={5}
             name="description"
@@ -77,8 +97,26 @@
             placeholder="Url"
 
         />
-        <br/>
 
+        <x-bladewind.input
+            id="slug"
+            name="slug"
+            required
+            placeholder="Slug"
+
+        />
+        <p style="color: red">
+            Pečlivě zkontroluj slug!
+            Slug je automaticky vygenerované slovo z názvu.
+            <br/>
+            Toto slovo bude použito pro identifikaci sekce v url
+            adrese.
+            <br/>
+            Např. "www.pruvodcestudenta.utb.cz/sekce/nazevSekce/_slug_".
+            <br/>
+            Slug musí být unikátní, slug nelze později změnit.
+        </p>
+        <br/>
         <x-bladewind.button size="tiny"
                             canSubmit="true">
             Add Topic
