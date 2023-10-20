d51e362a751fcc32673a5669842e8d592fc5a0fd
SedlarDavid <sedlardavid.cz@gmail.com>
2023-10-20 12:09:11 +0200

    fix AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,10 +25,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $this->app['request']->server->set('HTTPS','on');
 
         if (config('app.env') === 'production') {
+            $this->app['request']->server->set('HTTPS', 'on');
             URL::forceScheme('https');
+        } else {
+            URL::forceScheme('http');
+
         }
     }
 }
