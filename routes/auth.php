c191bcf4a5a4520d0421705ec1eb303dfe67ad88
Filip <fi.dlo@email.cz>
2023-11-22 13:41:59 +0100

    added spaces back

--- routes/auth.php
+++ routes/auth.php
@@ -12,7 +12,7 @@ use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function () {
-  /* Route::get('admin/register', [RegisteredUserController::class, 'create'])
+/*    Route::get('admin/register', [RegisteredUserController::class, 'create'])
                 ->name('register');
 
     Route::post('admin/register', [RegisteredUserController::class, 'store']);*/
