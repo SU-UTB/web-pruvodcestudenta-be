f48cdb936f908cdb63d4995f4651b999278a7bcd
SedlarDavid <sedlardavid.cz@gmail.com>
2024-06-09 14:24:45 +0200

    dashboard localization

--- resources/js/Pages/Dashboard.tsx
+++ resources/js/Pages/Dashboard.tsx
@@ -14,19 +14,19 @@ export default function Dashboard({auth, countOfSections, countOfTopics, countOf
                 <div className="mx-auto max-w-7xl px-6 lg:px-8">
                     <dl className="grid grid-cols-1 gap-x-8 gap-y-16 text-center lg:grid-cols-3">
                         <div className="mx-auto flex max-w-xs flex-col gap-y-4">
-                            <dt className="text-base leading-7 text-gray-600">Sections</dt>
+                            <dt className="text-base leading-7 text-gray-600">Sekcí</dt>
                             <dd className="order-first text-3xl font-semibold tracking-tight text-gray-900 sm:text-5xl">
                                 {countOfSections}
                             </dd>
                         </div>
                         <div className="mx-auto flex max-w-xs flex-col gap-y-4">
-                            <dt className="text-base leading-7 text-gray-600">Topics</dt>
+                            <dt className="text-base leading-7 text-gray-600">Článků</dt>
                             <dd className="order-first text-3xl font-semibold tracking-tight text-gray-900 sm:text-5xl">
                                 {countOfTopics}
                             </dd>
                         </div>
                         <div className="mx-auto flex max-w-xs flex-col gap-y-4">
-                            <dt className="text-base leading-7 text-gray-600">Locations</dt>
+                            <dt className="text-base leading-7 text-gray-600">Lokací</dt>
                             <dd className="order-first text-3xl font-semibold tracking-tight text-gray-900 sm:text-5xl">
                                 {countOfLocations}
                             </dd>
